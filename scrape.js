b79f2b40b0006461de4d6bd4c7e7f6e6c2b167e9
Austin Caron <aus10caron@icloud.com>
2018-09-28 00:48:12 -0400

    V0.2.0 - Scrape n' Search

--- scrape.js
+++ scrape.js
@@ -1,58 +1,52 @@
 const request = require('request');
 const cheerio = require('cheerio');
 
-const base_url = 'https://www.supremecommunity.com/season/fall-winter2018/droplists/';
 const url = 'https://www.supremecommunity.com';
+const base_url = 'https://www.supremecommunity.com/season/fall-winter2018/droplists/'; // Technically this is not static, as it will have to change with new seasons.
 
-const get_latest = (callback) => {
+const get_latest = (base_url, callback) => {
     request(base_url, (error, response, html) => {
-        if(error) {
-            console.log(error)
-        };
-        const $ = cheerio.load(html);
+        if(!error && response.statusCode == 200) {
+            var $ = cheerio.load(html);
+            var latest_url = $('.block');
 
-        return result = $('.block')
-        .first()
-        .attr('href');
-
-        // console.log(result);
-
-            
+            if(latest_url.is('h2') === false) {
+                callback(latest_url.first().attr('href'));
+            } else {
+                console.log('Tryin again');
+                callback(false);
+            }   
+        } else {
+            console.log(error);
+            callback(false);
+        }           
     });
 };
 
-get_latest();
+get_latest(base_url, function(result) {
+    var new_url = url + result
+    var get_items = (new_url, callback) => {
+        request(new_url, (error, response, html) => {
+            if(!error) {
+                var $ = cheerio.load(html);
+                const items = []
+                var itemCard =$('.card-details').map(function(i,e) {
+                    items[i] = $(this).text().replace(/\n/g,'').replace(/\s\s+/g, '').replace(/\$/g, ' -- $');
+                });
+                items.join(', ');
+                callback(items)
+            } else {
+                console.log(error);
+                callback(false);
+            }
 
-console.log(result);
+            // console.log(items)
+        })
+    };
+    get_items(new_url, function(items) {
+        for (let item of items) {
+            console.log(item);
+          }
+    });
 
-
-// const get_result = () => {
-//     request(url,(error, response, html, cb) => {
-//         if (!error && response.statusCode == 200) {
-//             const $ = cheerio.load(html);
-
-//             var result = $('.card-details').each((i, el, result) => {
-//                 const item = $(el)
-//                 .find('.card__body')
-//                 .text();
-
-//                 const price = $(el)
-//                 .find('.label-price')
-//                 .text();
-
-//                 var result = ("Item: " + item.trim() + " Price: " + price.trim());
-//                 return result
-//             });
-
-//          }
-
-// });
-// }
-
-// var new_result = $('.card-details').find('.card__body').text().replace(/\s\s+/g, '');
-// console.log(new_result);
-
-
-
-
-
+});
