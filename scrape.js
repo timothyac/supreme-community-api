5ac0761537d255a2176571c358781f45afb721b8
Austin Caron <aus10caron@icloud.com>
2019-06-02 23:24:47 -0400

    SC changed their markup

--- scrape.js
+++ scrape.js
@@ -1,9 +1,9 @@
 const cheerio = require("cheerio");
 const rp = require("request-promise");
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 
-const { asyncForEach } = require('./utils/asyncForEach')
-const { sleep } = require('./utils/sleep')
+const { asyncForEach } = require("./utils/asyncForEach");
+const { sleep } = require("./utils/sleep");
 
 let scraper = {};
 const options = {
@@ -14,80 +14,86 @@ const options = {
 };
 
 scraper.getLatestWeek = function() {
-    options.url = "https://www.supremecommunity.com/season/latest/droplists/"
-    return new Promise (function(resolve, reject) {
-        rp(options).then($ => {
-            // Search for elements with the block class
-            const latestURL = $('.block')
+  options.url = "https://www.supremecommunity.com/season/latest/droplists/";
+  return new Promise(function(resolve, reject) {
+    rp(options).then($ => {
+      // Search for elements with the block class
+      const latestURL = $(".block");
 
-            // If the element isn't a header, then resolve it
-            if(!latestURL.is('h2')) {
-                const href = (latestURL.first().attr('href'))
-                resolve(href)
-            } else {
-                reject("Couldn't find link")
-            }
-        })
-    })
-}
+      // If the element isn't a header, then resolve it
+      if (!latestURL.is("h2")) {
+        const href = latestURL.first().attr("href");
+        resolve(href);
+      } else {
+        reject("Couldn't find link");
+      }
+    });
+  });
+};
 
-scraper.grabNewItems = function (href) {
-    options.url = `https://www.supremecommunity.com/${href}`
-    return new Promise (function(resolve, reject) {
-        rp(options).then($ => {
-            // Define items as having a card-details class
-            const items = $('.card-details')
+scraper.grabNewItems = function(href) {
+  options.url = `https://www.supremecommunity.com/season/spring-summer2019/droplist/2019-05-30/`;
+  return new Promise(function(resolve, reject) {
+    rp(options).then($ => {
+      // Define items as having a card-details class
+      const items = $(".card-details");
 
-            // Array to store items
-            let list = []
+      // Array to store items
+      let list = [];
 
-            if(items) {
-                items.each((i, el) => {
-                    let obj = {}
-                    // Name of item
-                    obj.name = $(el).attr('data-itemname');
-                    // Code value that Supreme Community gives an item
-                    obj.code = $(el).attr('data-itemid');
-                    // Price of item
-                    obj.price = $(el).find(".label-price").text().trim();
-                    // Image Href of item
-                    let imageURL = $(el).find('img').attr('src');
-                    obj.image = `https://www.supremecommunity.com${imageURL}`
+      if (items) {
+        items.each((i, el) => {
+          let obj = {};
+          // Name of item
+          obj.name = $(el)
+            .find("h2")
+            .text();
+          // Code value that Supreme Community gives an item
+          obj.code = $(el).attr("data-itemid");
+          // Price of item
+          obj.price = $(el)
+            .find(".label-price")
+            .text()
+            .trim();
+          // Image Href of item
+          let imageURL = $(el)
+            .find("img")
+            .attr("src");
+          obj.image = `https://www.supremecommunity.com${imageURL}`;
 
-                    list.push(obj)
-                })
-                resolve(list)
-            } else {
-                reject("Couldn't find any items")
-            }
-        })
-    })
-}
+          list.push(obj);
+        });
+        resolve(list);
+      } else {
+        reject("Couldn't find any items");
+      }
+    });
+  });
+};
 
-scraper.sendToDiscord = function (list, webhook) {
-    // Chop up the user's webhook
-    const tokenAndID = webhook.slice(36).split('/')
+scraper.sendToDiscord = function(list, webhook) {
+  // Chop up the user's webhook
+  const tokenAndID = webhook.slice(36).split("/");
 
-    // Attach webhook to a new webhookClient
-    const client = new Discord.WebhookClient(tokenAndID[0], tokenAndID[1])
+  // Attach webhook to a new webhookClient
+  const client = new Discord.WebhookClient(tokenAndID[0], tokenAndID[1]);
 
-    // Loop through each item in the list
-    asyncForEach(list, async item => {
-        
-        // Build webhook embed
-        const embed = await new Discord.RichEmbed()
-        .setAuthor(item.name)
-        .setFooter("Supreme Community API")
-        .setTimestamp()
-        .setImage(item.image)
-        .setColor("#F00000")
+  // Loop through each item in the list
+  asyncForEach(list, async item => {
+    // Build webhook embed
+    const embed = await new Discord.RichEmbed()
+      .setAuthor(item.name)
+      .setFooter("Supreme Community API")
+      .setTimestamp()
+      .setImage(item.image)
+      .setColor("#F00000");
 
-        // Send to server
-        await client.send(embed)
-        
-        // Sleep for 1.5 sec, that way discord doesn't rate limit
-        await sleep(1500)
-    })
-}
+    // Send to server
+    await client.send(embed);
 
-module.exports = scraper
+    // Sleep for 1.5 sec, that way discord doesn't rate limit
+    await sleep(1500);
+  });
+};
+
+module.exports = scraper;
